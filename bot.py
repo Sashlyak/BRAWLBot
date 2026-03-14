d4282ef38863a3a25f0cdf5678544fd406e9feca
Sashlyak <sasacorpita55@gmail.com>
2026-03-14 22:54:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
-TOKEN = os.environ.get('8158874437:AAFiUlWfnct9eezmCbjrqJLRoeGgYexmGfQ')
+TOKEN = os.environ.get('TOKEN')
 
 user_data_store = {}
 
